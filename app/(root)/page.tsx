ab0467bfd2c51b0319b161af2d6e05a6e8ffc09c
Kabir Berger <kjb22@st-andrews.ac.uk>
2024-10-06 19:18:56 -0400

    Implement complete layout.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const Home = () => {
-  return <div>Home</div>
+  const loggedIn = {
+    firstName: 'Kabir',
+    lastName: 'Berger',
+    email: 'bergerkabir@gmail.com',
+  }
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and trasactions efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.0 }]}
+      />
+    </section>
+  )
 }
 
 export default Home
